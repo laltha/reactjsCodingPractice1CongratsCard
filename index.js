4e974eab1a2a010ce21c1d16456dd6484612f454
lalitha <lalithalikes@gmail.com>
2024-02-28 11:54:02 +0000

    addsReactjsCongratsCard

---
+++ index.js
@@ -0,0 +1,16 @@
+const element = (
+  // Write your code here.
+  <div className='container'>
+    <div>
+      <h1>Congratulations</h1>
+    </div>
+    <div className='smallbox'>
+      <img src='https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png' />
+      <h1>Kiran V</h1>
+      <p1>Vishnu Institute Of Computer Education And Technology. Bhimavaram</p1>
+      <img src='https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png' />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
